999ff829aff2a0cc9f8c62be0b602f6a2fbaa479
RGood <mullan17@gmail.com>
2017-09-12 18:22:30 -0400

    Minimally functional

--- thebox/thebox.py
+++ thebox/thebox.py
@@ -5,21 +5,25 @@ import webbrowser
 from flask import Flask, request
 from threading import Thread
 from random import Random
+import configparser
 #==================================================End Config======================================================
 #==================================================OAUTH APPROVAL==================================================
 app = Flask(__name__)
 
-CLIENT_ID = '24s3PNTHFbHsoA' #SET THIS TO THE ID UNDER PREFERENCES/APPS
-CLIENT_SECRET = '5HuUzRcISFNWza5Bm-CkHipm0-A' #SET THIS TO THE SECRET UNDER PREFERENCES/APPS
-owner_scope = 'identity modothers' #SET THIS. SEE http://praw.readthedocs.org/en/latest/pages/oauth.html#oauth-scopes FOR DETAILS.
+Config = configparser.ConfigParser()
+Config.read('box_info.cfg')
+
+CLIENT_ID = Config.get('Reddit Access','cid')
+CLIENT_SECRET = Config.get('Reddit Access','csec')
+REDIRECT_URI = Config.get('Reddit Access','callback')
+
+owner_scope = 'identity modothers read' #SET THIS. SEE http://praw.readthedocs.org/en/latest/pages/oauth.html#oauth-scopes FOR DETAILS.
 participant_scope = 'identity modself'
 
-owner = ''
+owner = None
 subreddit = ''
-temp_mods = 5
+temp_mods = int(Config.get('Reddit Access','mods'))
 rand = Random()
-
-REDIRECT_URI = 'http://127.0.0.1:65010/authorize_callback'
 
 #Kill function, to stop server once auth is granted
 def kill():
@@ -32,7 +36,7 @@ def kill():
 #Callback function to receive auth code
 @app.route('/authorize_callback')
 def authorized():
-	if(r.user.me()):
+	if(owner != None):
 		return auth_participant(praw.Reddit(
 			client_id=CLIENT_ID,
 			client_secret=CLIENT_SECRET,
@@ -42,28 +46,35 @@ def authorized():
 	else:
 		return auth_owner(owner_client, request.args.get('code', ''))
 
+@app.route('/')
+def get_auth_route():
+	auth_url = owner_client.auth.url(participant_scope.split(' '), True)
+	return '<meta http-equiv="refresh" content="0; url='+auth_url+'" />\
+			<p><a href="'+ auth_url +'">Redirect</a></p>'
+
 def auth_owner(client, code):
-	global owner
+	global owner, subreddit
 	client.auth.authorize(code)
-	owner = r.user.me().name
-	text = 'The Box started on account /u/'+user.name
+	owner = client.user.me().name
+	subreddit = client.subreddit(Config.get('Reddit Access','subreddit'))
+	text = 'The Box started on account /u/'+owner
 	return text
 
 def auth_participant(client, code):
 	client.auth.authorize(code)
 	mod_user(client)
-	return '<meta http-equiv="refresh" content="0; url=http://www.reddit.com/r/thebox/about/moderators" />\
-			<p><a href="http://www.reddit.com/r/thebox/about/moderators">Redirect</a></p>'
+	return '<meta http-equiv="refresh" content="0; url=http://www.reddit.com/r/'+ subreddit.display_name +'/about/moderators" />\
+			<p><a href="http://www.reddit.com/r/'+ subreddit.display_name +'/about/moderators">Redirect</a></p>'
 
 def mod_user(participant_client):
 	#check mods with owner client
 	#filter out owner so you don't demod yourself
-	mods = list(filter((lambda user: user.name!=owner), subreddit.moderator)
+	mods = list(filter((lambda user: user.name!=owner), subreddit.moderator()))
 	
 	#maybe remove
-	while(len(mods) > temp_mods):
+	while(len(mods) >= temp_mods):
 		to_remove = mods[rand.randint(0, len(mods))]
-		print("Removing " + to_remove.name " of possible:")
+		print("Removing " + to_remove.name + " of possible:")
 		print(mods)
 		subreddit.moderator.remove(to_remove)
 		mods.remove(to_remove)
@@ -75,7 +86,7 @@ def mod_user(participant_client):
 	#accept user with participant client
 	print("Accepting for " +participant_client.user.me().name)
 	temp_sub = participant_client.subreddit(subreddit.display_name)
-	temp_sub.moderator.add(participant_client.user.me())
+	temp_sub.mod.accept_invite()
 
 owner_client = praw.Reddit(
 	client_id=CLIENT_ID,
@@ -84,6 +95,9 @@ owner_client = praw.Reddit(
 	user_agent='The Box Bot'
 	)
 
-webbrowser.open(r.auth.url(scope.split(' '),True))
+print("Owner Auth")
+print(owner_client.auth.url(owner_scope.split(' '),True))
+print("User Auth")
+print(owner_client.auth.url(participant_scope.split(' '), True))
 app.run(debug=False, port=65010)
 #==================================================END OAUTH APPROVAL-=============================================
